c97407c99940e893634d35c357a8b181218aff90
Francisco Racciatti <mokenuf@me.com>
2026-04-04 17:18:20 -0300

    Update nuxt.config.ts

--- apps/trainer/nuxt.config.ts
+++ apps/trainer/nuxt.config.ts
@@ -1,6 +1,6 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  modules: ['@nuxt/ui', '@nuxtjs/supabase', '@vueuse/nuxt'],
+  modules: ['@nuxt/ui', '@nuxtjs/supabase'],
   css: ['~/assets/css/main.css'],
   compatibilityDate: '2025-07-15',
   devtools: { enabled: true },
